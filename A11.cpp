9ee73e73a90c8e254645b49cb97acf1760f22802
Your Name <you@example.com>
2023-06-03 12:21:24 +0900

    A11

--- A11.cpp
+++ A11.cpp
@@ -4,23 +4,19 @@ using namespace std;
 int a[100009];
 
 int main() {
-    int n,x;
-    cin >> n >> x;
-    for (int i = 0; i < n; i++) {
+    int n;
+    cin >> n;
+    for (int i = 1; i <= n; i++) {
         cin >> a[i];
     }
-
-
-    int left, right, mid;
-    left = 0;
-    right = n-1;
-    while (true) {
-        mid = (left+right)/2;
-        if (a[mid] > x) right = mid -1;
-        else if (a[mid] < x) left = mid + 1;
-        else {
-            cout << mid+1 << endl;
-            return 0;
-        }
+    int q;
+    cin >> q;
+    // cout << "aaa" << endl;
+    for (int i = 0; i < q; i++) {
+        int x;
+        cin >> x;
+        sort(a+1, a+n+1);
+        int ans = lower_bound(a+1, a+n+1, x) -a;
+        cout << ans-1 << endl;
     }
 }
