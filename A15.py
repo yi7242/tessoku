15a52c1e00e00d7123b72841acafa976a28ea07a
Your Name <you@example.com>
2023-06-13 15:28:26 +0900

    B17

--- A15.py
+++ A15.py
@@ -1,9 +1,10 @@
-import bisect
+import math
 n = int(input())
 a = list(map(int, input().split()))
-choose = sorted(set(a))
-b = []
-for i in range(n):
-    x = bisect.bisect(choose, a[i])
-    b.append(x)
-print(*b)
+b = list(map(int, input().split()))
+dp = [math.inf] * n
+for i in range(n-1):
+    if i < n-2:
+        dp[i+2] = min(dp[i+2], dp[i] + b[i])
+    dp[i+1] = min(dp[i+1], dp[i]+a[i])
+print(dp)
