bf4d37087a13c07751107a550833aad862f53524
Your Name <you@example.com>
2023-06-17 11:40:34 +0900

    aaa

--- test.py
+++ test.py
@@ -1,31 +1,9 @@
-n = int(input())
-p = list(map(int, input().split()))
+import numpy as np
+def pred(plants, query):
+    for t, x, y in query:
+        if t == 0:
+            plants = np.where(plants <= x, plants + y, plants)
+        else:
+            plants = np.where(plants >= x, plants - y, plants)
 
-sp = sorted(p)
-l1 = []
-l2 = []
-while sp != p:
-    cantb = True
-    for i in range(n-2):
-        if p[i+2] < p[i]:
-            cantb = False
-            depo = p[i]
-            p[i] = p[i+2]
-            p[i+2] = depo
-            l1.append("B")
-            l2.append(i)
-    if not cantb:
-        continue
-    else:
-        for i in range(n-1):
-            if p[i+1] < p[i]:
-                depo = p[i]
-                p[i] = p[i+1]
-                p[i+1] = depo
-                l1.append("A")
-                l2.append(i)
-                continue
-# print(p)
-print(len(l1))
-for i in range(len(l1)):
-    print(l1[i], l2[i]+1)
+    return plants
