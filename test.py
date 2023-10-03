d84728f756aba88c1e287a9a7f37d0ccd6b305c5
Your Name <you@example.com>
2023-10-03 15:42:50 +0900

    sdfsaf

--- test.py
+++ test.py
@@ -1,56 +1,45 @@
+# 深さ優先探索（行きがけ）
+import sys
+input = sys.stdin.readline
 from collections import deque
-def my_index(l, x, default=False):
-    if x in l:
-        return l.index(x)
-    else:
-        return default
 
-snuke = list("snuke")
+# グラフの作成(無向グラフでは#を消す)
+N = int(input())
+graph = [deque([]) for _ in range(N + 1)]
+for _ in range(N):
+    u, k, * v = [int(x) for x in input().split()] # uは頂点番号、kは隣接頂点の個数
+    v.sort()
+    for i in v:
+        graph[u].append(i)
+        # graph[i].append(u) # 無向グラフ
 
-#探索関数：ゴールしたらそのときの位置・移動数を返す
-def Maze(start):
-    #スタート位置（x座標, y座標, 移動回数）をセット
-    pos = deque(start)
+time = 0
+arrive_time = [-1] * (N + 1) # 到着時刻
 
-    while len(pos) > 0:#探索可能ならTrue
-        x, y, index = pos.popleft() #リストから探索する位置を取得
+# 深さ優先探索
+def dfs(v):
+    global time
+    time += 1
+    stack = [v]
+    arrive_time[v] = time
+    while stack:
+        v = stack[-1]
+        if graph[v]:
+            w = graph[v].popleft()
+            if arrive_time[w] < 0:
+                time += 1
+                arrive_time[w] = time
+                stack.append(w) 
+        else:
+            stack.pop()          
+    return arrive_time
 
-        #ゴールについた時点で終了
-        if x == h and y == w:
-            return True
+# 孤立している頂点対策
+for i in range(N):
+    if arrive_time[i + 1] < 0:
+        ans = dfs(i + 1)
 
-        #探索済みとしてセット
-        maze[x][y] = 9
-        # print(x,y, index)
-        next = (index + 1)%5
-        #現在位置の上下左右を探索：〇<2は壁でもなく探索済みでもないものを示す
-        if maze[x-1][y] == next:#左
-            pos.append([x-1, y, next])
-        if maze[x+1][y] == next:
-            pos.append([x+1, y, next])
-        if maze[x][y-1] == next:
-            pos.append([x, y-1, next])
-        if maze[x][y+1] == next:
-            pos.append([x, y+1, next])
-
-    return False
-h,w= map(int, input().split())
-maze = []
-maze.append([9]*(w+2))
-for i in range(h):
-    s = list(input())
-    x=[9]
-    for j in s:
-        x.append(my_index(snuke, j, 9))
-    x.append(9)
-    maze.append(x)
-maze.append([9]*(w+2))
-# print(maze)
-start = [[1, 1, 0]]     #スタート位置
-
-result = Maze(start)  #探索
-
-if result:
-    print("Yes")
-else:
-    print("No")
+# 頂点番号、到着時刻
+for j in range(N):
+    temp = [j + 1, ans[j + 1]]
+    print(* temp)
